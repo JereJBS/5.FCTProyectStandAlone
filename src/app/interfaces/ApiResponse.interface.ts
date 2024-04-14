7bac939c8f3b6d850fbe8bf925460bd54e8d754f
JereJBS <jerestudiios@gmail.com>
2024-04-14 17:54:13 +0100

    added register service

---
+++ src/app/interfaces/ApiResponse.interface.ts
@@ -0,0 +1,5 @@
+export interface ApiResponse {
+
+    username:"",
+    password:""
+}
--- src/app/services/db-service.service.ts
+++ src/app/services/db-service.service.ts
@@ -1,9 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { RegisterInterface } from '../interfaces/register.interface';
+import { Observable } from 'rxjs';
+import { ApiResponse } from '../interfaces/ApiResponse.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DbServiceService {
 
-  constructor() { }
+  private registerEndpoint = "http://localhost:8081/auth/register";
+
+  constructor(private http: HttpClient) { }
+
+  Register(form: RegisterInterface): Observable<ApiResponse>{
+    return this.http.post<ApiResponse>(this.registerEndpoint, form)
+  }
 }
