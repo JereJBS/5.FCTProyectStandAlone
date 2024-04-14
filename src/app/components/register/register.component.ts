57fcdf4faa658c8bb74134c9692b76d9bd9c35a7
JereJBS <jerestudiios@gmail.com>
2024-04-14 17:40:17 +0100

    added service structure

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,12 +1,22 @@
+import { HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, HttpClientModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+
+  FormRegister = {
+    username : "",
+    password :"",
+    firstname: "",
+    lastname: "",
+    country: "",
+  }
 
 }
