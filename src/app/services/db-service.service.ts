8704d45dc4e685de772f7dd1bd459d3ce93d24be
JereJBS <jerestudiios@gmail.com>
2024-04-14 19:30:04 +0100

    login service done

--- src/app/services/db-service.service.ts
+++ src/app/services/db-service.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { RegisterInterface } from '../interfaces/Register.interface';
 import { Observable } from 'rxjs';
 import { ApiResponse } from '../interfaces/ApiResponse.interface';
+import { LoginInterface } from '../interfaces/Login.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -10,10 +11,16 @@ import { ApiResponse } from '../interfaces/ApiResponse.interface';
 export class DbServiceService {
 
   private registerEndpoint = "http://localhost:8081/auth/register";
+  private loginEndpoint = "http://localhost:8081/auth/login";
 
   constructor(private http: HttpClient) { }
 
   Register(form: RegisterInterface): Observable<ApiResponse>{
     return this.http.post<ApiResponse>(this.registerEndpoint, form)
   }
+
+  Login(form: LoginInterface): Observable<ApiResponse>{
+    return this.http.post<ApiResponse>(this.loginEndpoint, form)
+  }
+
 }
